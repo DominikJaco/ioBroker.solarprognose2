828615a5fbe99163cd68c43cead65076d2aa5bb2
DominikJaco <34600835+DominikJaco@users.noreply.github.com>
2025-06-22 03:57:06 +0200

    i18n

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -1,8 +1,11 @@
-module: {
-  rules: [
-    {
-      test: /\.json$/,
-      loader: 'json-loader'
-    }
-  ]
-}
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+        type: 'javascript/auto'
+      }
+    ]
+  }
+};
